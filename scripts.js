2e39e4bcfcb86a4219f33c266cffdc4725184ad0
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2017-10-24 21:20:11 +0300

    lesson 6

---
+++ scripts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var op1 = document.getElementById('op1'),
+    op2 = document.getElementById('op2'),
+    result = document.getElementById('result'),
+    buttons = document.getElementsByTagName('button'),
+    i;
+
+function onClick(event) {
+    switch (event.target.innerHTML) {
+        case '+': result.innerHTML = parseFloat(op1.value) + parseFloat(op2.value); break;
+        case '-': result.innerHTML = parseFloat(op1.value) - parseFloat(op2.value); break;
+        case '*': result.innerHTML = parseFloat(op1.value) * parseFloat(op2.value); break;
+        case '/': result.innerHTML = parseFloat(op1.value) / parseFloat(op2.value); break;
+    }
+}
+
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = onClick;
+}
